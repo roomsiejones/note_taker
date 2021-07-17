98de2573513455a02eccd476da7b8867a6f1947f
Roomsie Jones <seangravito@gmail.com>
2021-07-17 15:07:29 -0500

    added paths to /notes and /api/notes and changed the / path to a * path to catch anything that isnt already defined for the get commands... found out it needs to be lower than other paths to function correctly.

--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,26 @@
 // needed to use the libraries
 const express = require('express');
 const fs = require('fs');
-
+const noteData = require('./db/db.json');
 const app = express();
 
 const PORT = process.env.PORT || 3000
-// ASK tutor what this does exactly, Jung told us to use this, but...
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const path = require('path');
+// need this line to access the css files and 'static' files in the public folder
+app.use(express.static('public'));
+
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')))
+
+app.get('/api/notes', (req, res) => res.json(noteData));
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 app.listen(PORT, () => {
     console.log(`listening on PORT: ${PORT}`);
   });
+
