0fe4ef95e707332cb82204284e443b1b9da030ae
Roomsie Jones <seangravito@gmail.com>
2021-07-17 15:41:08 -0500

    Added post for the api, and the notes do get saved -- cannot retrieve yet...

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -4,5 +4,10 @@ module.exports = (app) => {
 
     app.get('/api/notes', (req, res) => res.json(noteData));
 
+    app.post('/api/notes', (req, res) => {
+    noteData.push(req.body);
+    res.json(true);
+    
+    });
 
 }
