3e459bd27f18e134253a9d0c8c6ad3e356049207
Roomsie Jones <seangravito@gmail.com>
2021-07-17 16:59:07 -0500

    made the ids get stored and now the notes can be retrieved

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,13 +1,16 @@
-const noteData = require('../db/db.json');
+let noteData = require('../db/db.json');
 
 module.exports = (app) => {
+    
 
     app.get('/api/notes', (req, res) => res.json(noteData));
 
+    // stores the data in the json and assigns id's based on where it is in the file
     app.post('/api/notes', (req, res) => {
-    noteData.push(req.body);
-    res.json(true);
-    
+        noteData.push({...req.body, id: noteData.length});
+        res.json(true);
     });
+   
+
 
 }
--- Develop/server.js
+++ Develop/server.js
@@ -8,18 +8,14 @@ const PORT = process.env.PORT || 3000
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// const path = require('path');
 // need this line to access the css files and 'static' files in the public folder
 app.use(express.static('public'));
 
+// put routes in separate files to make routing 'easier' in future iterations
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
-// app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')))
 
-// app.get('/api/notes', (req, res) => res.json(noteData));
-
-// app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 app.listen(PORT, () => {
     console.log(`listening on PORT: ${PORT}`);
